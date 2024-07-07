4ae3eefaa130c8ef721c59e9210138343c718d4c
Scott Sedgwick <scott.sedgwick@gmail.com>
2024-07-07 17:14:02 +1000

    Added more data for Zoe

--- src/Data/Zoe.elm
+++ src/Data/Zoe.elm
@@ -5,5 +5,10 @@ import Model exposing (Weight, mkdate)
 
 weight : List Weight
 weight = 
-  [ Weight (mkdate 2024 Jul  7)  76.1
+  [ Weight (mkdate 2022 Feb  5)  73.0
+  , Weight (mkdate 2022 Feb 19)  73.7
+  , Weight (mkdate 2022 Mar  6)  74.7
+  , Weight (mkdate 2022 Aug 27)  80.2
+  , Weight (mkdate 2023 Apr  3)  82.2
+  , Weight (mkdate 2024 Jul  7)  76.1
   ]
